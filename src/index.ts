9bf48248cf40b24f619f00e167d1798c433db61c
Pierre Fouquet <90923973+PierreFouquet@users.noreply.github.com>
2025-08-28 17:08:48 +0100

    Add llm models

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { llm_settings, llm_prompts } from '../public/config.js';
+import { llm_models, llm_settings, llm_prompts } from '../public/config.js';
 
 // The Durable Object that will handle the analysis state.
 export interface AnalysisObjectState {
